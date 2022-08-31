d2e3262373400cf917d5c2d8c1400c8fe3ca4ac5
«Antonina <a1523@mail.ru>
2022-08-31 19:23:11 +0300

    Задача 4

---
+++ MyProject4/Program4.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, к-я на вход принимает число (N), а на выходе показывает все целые числа в промежуьке от -N до N
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+// Определяем противоположное число. Что бы учесть вариант, что пользователь уже внес число со знаком "-", используем 
+// библиотеку Math и функцию Abs (взятие числа по модулю)
+int startNumber = Math.Abs(number) * -1;
+
+while (startNumber <= number) {
+    Console.WriteLine(startNumber);
+    startNumber = startNumber + 1;
+}
