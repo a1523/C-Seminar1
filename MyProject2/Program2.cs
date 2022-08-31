2b3a26bc2ec0326a24fed4c5cd9a6e6d45c1a518
«Antonina <a1523@mail.ru>
2022-08-31 18:38:07 +0300

    задача2

---
+++ MyProject2/Program2.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет проверять является ли второе число корнем первого
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int result = Convert.ToInt32(Math.Pow(number1, 2));
+
+if (result == number2)
+{
+    Console.WriteLine ($"Второе число {number2} является корнем первого числа {number1}");
+}
+else
+{
+    Console.WriteLine ($"Второе число {number2} не является корнем первого числа {number1}");
+}
